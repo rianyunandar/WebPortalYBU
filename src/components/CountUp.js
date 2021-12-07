2cb5f21d52f3ab4dd7da11cd355ebdbca4027ddc
rianyunandar <rianyunandar@gmail.com>
2021-12-07 14:29:53 +0700

    update Count Up

--- src/components/CountUp.js
+++ src/components/CountUp.js
@@ -5,8 +5,8 @@ function Footer() {
   return (
     <div>
       <h1 class="text-center">
-        <CountUp start={0} end={6} duration={6}/> Jurusan <CountUp start={0}  end={1050} duration={6} /> Murid{" "}
-        <CountUp start={0}  end={9000} duration={6}/> Alumni{" "}
+        <CountUp start={0} end={6} duration={3}/> Jurusan <CountUp start={0}  end={1050} duration={3} /> Murid{" "}
+        <CountUp start={0}  end={9000} duration={3}/> Alumni{" "}
       </h1>
     </div>
   );
