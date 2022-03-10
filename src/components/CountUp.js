cb73ff8b0e132528d88cb1ad57cc4472cb145069
rianyunandar <rianyunandar@gmail.com>
2022-03-10 15:29:14 +0700

    update jumlah alummi

--- src/components/CountUp.js
+++ src/components/CountUp.js
@@ -5,11 +5,11 @@ function CountUP() {
   return (
     <div>
       <h1 class="CountText">
-          <CountUp start={0} end={6} duration={3} />{" "}
-        Jurusan <CountUp start={0} end={1050} duration={3} /> Murid{" "}
+        <CountUp start={0} end={6} duration={3} /> Jurusan{" "}
+        <CountUp start={0} end={1050} duration={3} /> Murid{" "}
       </h1>
       <h1 class="CountText">
-        <CountUp start={0} end={9000} duration={3} /> Alumni
+        &gt <CountUp start={0} end={25000} duration={3} /> Alumni
       </h1>
     </div>
   );
